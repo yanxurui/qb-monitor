84cd70a99f0c36f424feb45dfaa7503b9aae8bd9
Xurui Yan <xuryan@microsoft.com>
2023-03-03 20:08:20 +0800

    Add: Vue

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import time
 import logging
 from datetime import datetime
@@ -14,28 +15,18 @@ logging.basicConfig(
 routes = web.RouteTableDef()
 
 @routes.get('/')
+async def index(request):
+    directory = request.app.router['static'].get_info()['directory']
+    location = os.path.join(directory, 'index.html')
+    return web.FileResponse(path=location)
+
+@routes.get('/qbs')
 async def home(request):
-    start = time.time()
-
-    app.logger.info('Started')
-
     # reload config if necessary
     config = Config.get_config(True)
+    return web.json_response(config['qbs'])
 
-    # set prefix
-    prefix = request.headers.get('X-Forwarded-Prefix', '').rstrip('/')
-    data = {
-        "qb_info": config['qbs'],
-        "now": datetime.now().strftime("%H:%M:%S"),
-        "url_prefix": prefix
-    }
-    response = aiohttp_jinja2.render_template("home.html", request, context=data)
-    end = time.time()
-    duration = end - start
-    app.logger.info('Finishied in {0:.2f} seconds'.format(duration))
-    return response
-
-@routes.get(r'/{qb_id:\d+}')
+@routes.get(r'/qbs/{qb_id:\d+}')
 async def get_qb_stats(request):
     try:
         qb_id = int(request.match_info['qb_id'])
@@ -90,25 +81,17 @@ async def query_qb(qb):
                 app.logger.error('failed to get transfer info {}. Status code is {}'.format(url, r.status))
                 return False
             j = await r.json()
-            qb['dl_info_speed'] = sizeof_fmt(j['dl_info_speed'])
-            qb['dl_info_data'] = sizeof_fmt(j['dl_info_data'])
-            qb['up_info_speed'] = sizeof_fmt(j['up_info_speed'])
-            qb['up_info_data'] = sizeof_fmt(j['up_info_data'])
+            qb['dl_info_speed'] = j['dl_info_speed']
+            qb['dl_info_data'] = j['dl_info_data']
+            qb['up_info_speed'] = j['up_info_speed']
+            qb['up_info_data'] = j['up_info_data']
             end = time.time()
             app.logger.info('Finished {0} in {1:.2f} seconds'.format(url, end-start))
             return True
 
-def sizeof_fmt(num, suffix="B"):
-    for unit in ["", "K", "M", "G", "T", "P", "E", "Z"]:
-        if abs(num) < 1024.0:
-            return f"{num:3.1f}{unit}{suffix}"
-        num /= 1024.0
-    return f"{num:.1f}Yi{suffix}"
-
 app = web.Application()
 app.add_routes(routes)
-app.router.add_static('/static/', path='static', name='static')
-app['static_root_url'] = '/static'
+app.router.add_static('/', path='static', name='static')
 aiohttp_jinja2.setup(app,
     loader=jinja2.FileSystemLoader('templates'))
 
